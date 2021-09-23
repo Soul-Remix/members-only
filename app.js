ea6638f5732fb4e4cab85288ad3e8c541152f248
Soul-Remix <soulremix-webdev@pm.me>
2021-09-23 17:31:41 +0300

    add mongo-connect to save session

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
+const MongoStore = require('connect-mongo');
 require('dotenv').config();
 
 const indexRouter = require('./routes/index');
@@ -27,11 +28,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Session setup
+
+const sessionStore = MongoStore.create({
+  mongoUrl: process.env.MONGO_URI2,
+  autoRemove: 'native',
+  ttl: 10 * 24 * 60 * 60,
+});
+
 app.use(
   session({
     secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
+    store: sessionStore,
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000 * 10,
+    },
   })
 );
 
